2b4cc8d1ffb7bc5ac9dc425e462ce047e4664a17
Cool BRG <budhram.gurung01@gmail.com>
2021-09-16 11:14:47 +0530

    Add home page and setup images in webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "stylesheets/application" // Add this line
+import "stylesheets/application"
+const images = require.context('../images', true) // images
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
