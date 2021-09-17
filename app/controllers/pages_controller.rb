88613640c28ab58f928e5325de4f6087c5f2f9e8
Cool BRG <budhram.gurung01@gmail.com>
2021-09-17 17:39:00 +0530

    Added contributors page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def contributors
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  get '/contributors', to: 'pages#contributors'
 end
